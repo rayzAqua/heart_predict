75bffc991c242ba4318c03e9bf523d54a52ba752
PhanVanHieu-Ptit <96578636+HieuPit@users.noreply.github.com>
2023-04-17 23:14:06 +0700

    fix config

---
+++ routers/index.js
@@ -0,0 +1,24 @@
+function hieu56(app) {
+  //viet route trong nay
+}
+function hieu57(app) {
+  //viet route trong nay
+}
+function hoa(app) {
+  //viet route trong nay
+}
+
+function route(app) {
+  hieu56(app);
+  hieu57(app);
+  hoa(app);
+
+  //API chung
+  //   app.use("/site", siteRouter);
+  app.use("/", (req, res) => {
+    res.send("Hello");
+  });
+}
+
+// module.exports = route;
+export default route;
