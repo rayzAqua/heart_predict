d295756bb8ca274224c1069410ba0258ec4f1c4c
ducmanhlai <ducmanhlai22@gmail.com>
2023-04-19 15:26:00 +0700

    create, update and get data

--- controllers/history_controller.js
+++ controllers/history_controller.js
@@ -1,6 +1,45 @@
-
-function getHistory(req,res,next){   
-    console.log(req.user)
-    res.send({mess:'ok'})
+import Account from "../models/Account.js";
+import Data from "../models/Data.js";
+class HistoryController {
+  async getHistory(req, res, next) {
+    const user = JSON.parse(req.user);
+    const data = await Data.findOne({ userInfo: user._id });
+    res.status(200).json({
+      status: true,
+      message: "Thành công",
+      data: data,
+    })
+  }
+  async createHistory(req, res) {
+    const user = JSON.parse(req.user);
+    const { heart, spO2 } = req.body;
+    var date = new Date();
+    const data = await Data.findOne({ userInfo: user._id });
+    if (data == null) {
+      const acccount = await Data.create({
+        userInfo: user,
+        heart: [{ heartRate: heart, date: date }],
+        spO2: [{ oxygen: spO2, date: date }]
+      })
+    }
+    else {
+      Data.updateOne({ userInfo: user._id },
+        {
+          heart: [...data.heart, { heartRate: heart, date: date }],
+          spO2: [...data.spO2, { oxygen: spO2, date: date }]
+        }).catch(err =>res.status(200).json({
+          status: 'fail',
+          message: "Có lỗi xảy ra",
+          data: {},
+        })
+        )
+    }
+    res.status(200).json({
+      status: true,
+      message: "Thành công",
+      data: {},
+    })
+  }
 }
-export default getHistory;
+
+export default new HistoryController();
--- routers/history.routes.js
+++ routers/history.routes.js
@@ -1,6 +1,7 @@
 import authenticateToken from "../middlewares/authentication.js";
-import getHistory from "../controllers/history_controller.js";
+import HistoryController from "../controllers/history_controller.js";
 import express from "express";
 const historyRouter = express.Router();
-historyRouter.get("/",authenticateToken,getHistory);
+historyRouter.post("/create",authenticateToken,HistoryController.createHistory);
+historyRouter.get("/get",authenticateToken,HistoryController.getHistory);
 export default historyRouter;
