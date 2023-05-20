60ec567f4b1bd62be680e2269c8ce44c8662d9ac
PhanVanHieu-Ptit <96578636+HieuPit@users.noreply.github.com>
2023-05-20 09:00:37 +0700

    fix route create data

--- routers/data.routes.js
+++ routers/data.routes.js
@@ -4,6 +4,6 @@ import DataControllers from "../controllers/data_controller.js";
 const dataRouter = express.Router();
 
 dataRouter.get("/latest/:id", DataControllers.getLatestData);
-dataRouter.post("/", DataControllers.createData);
+dataRouter.post("/create", DataControllers.createData);
 
 export default dataRouter;
