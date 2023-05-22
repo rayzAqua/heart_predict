490d6aff4b93993661cd9719b60b32b5862fd34a
PhanVanHieu-Ptit <96578636+HieuPit@users.noreply.github.com>
2023-05-22 16:08:37 +0700

    fix bug

--- controllers/data_controller.js
+++ controllers/data_controller.js
@@ -3,16 +3,19 @@ import Account from "../models/Account.js";
 import Data from "../models/Data.js";
 
 class DataControllers {
-  async createData(req, res, next) {  
+  async createData(req, res, next) {
     try {
       const { id, heartRate, SpO2, temp } = req.body;
-      // const dateAdded = new Date().toISOString();      
-      const userInfo = await Account.findOne({ _id: new mongoose.Types.ObjectId(id)}).populate("userInfo");
+      // const dateAdded = new Date().toISOString();
+
+      const userInfo = await Account.findOne({
+        _id: new mongoose.Types.ObjectId(id),
+      }).populate("userInfo");
       const newData = await Data.create({
         userInfo,
         heartRate,
         SpO2,
-        temp        
+        temp,
       });
       res.status(200).json(newData);
     } catch (err) {
